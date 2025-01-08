4bc542537b09435a83f3551ef29ded1c6ff9b1ee
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-01-08 09:47:37 -0300

    cleaned up comments

--- main.go
+++ main.go
@@ -2,21 +2,17 @@ package main
 
 import "fmt"
 
-// List represents a singly-linked list that holds
-// values of any type.
 type List[T any] struct {
 	next *List[T]
 	val  T
 }
 
-// prints the list
 func (lst *List[T]) String() string {
 	if lst == nil {
 		return "<nil>"
 	}
 	return fmt.Sprintf("{val: %v, next: %s}", lst.val, lst.next)
 }
-
 func createList[E any](val E) *List[E] {
 	newList := new(List[E])
 	newList.val = val
